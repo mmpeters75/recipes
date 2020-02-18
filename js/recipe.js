9077a337b29b78419da9d1990e96799d495e9a59
mmpeters75 <matthewpeters@MacBook-Pro-3.local>
2020-02-18 18:28:17 -0500

    a couple o pies

--- js/recipe.js
+++ js/recipe.js
@@ -1,13 +1,20 @@
 const printButton = document.querySelector('.printButton');
 const recipeImage = document.querySelector('.recipeImage');
+const recipeImage2 = document.querySelector('.recipeImage2');
 if (printButton !== null && printButton !== undefined) {
   printButton.addEventListener("click", function() {
       printButton.style.display = "none";
       recipeImage.style.display = "none";
+      if (recipeImage2) {
+        recipeImage2.style.display = "none";
+      }
       window.print();
       window.onfocus=function(){ 
         printButton.style.display = "inline-block";
         recipeImage.style.display = "block";
+        if (recipeImage2) {
+          recipeImage2.style.display = "block";
+        }
       }
   });
 }
